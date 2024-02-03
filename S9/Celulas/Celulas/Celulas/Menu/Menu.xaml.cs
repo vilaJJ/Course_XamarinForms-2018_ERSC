f351adcdb8caf4935e6949b3401eee533fdffb57
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2024-02-03 04:21:37 -0300

    1/13 Seção 9: [ESTUDO] Celulas

---
+++ S9/Celulas/Celulas/Celulas/Menu/Menu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Celulas.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : FlyoutPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_1_Clicked(object sender, EventArgs e)
+        {
+            // Detail = new
+        }
+    }
+}
